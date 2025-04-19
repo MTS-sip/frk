22cf3f439d207d2a9b69e56eadf8b5e6ff4cf0e0
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-04-19 11:25:50 -0400

    login steps

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,10 +12,35 @@ import {
 
 //Render deployment
 console.log("GraphQL URI is:", import.meta.env.VITE_GRAPHQL_URI);
+
+import { setContext } from '@apollo/client/link/context';
+import { createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URI || '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+/*
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHQL_URI || '/graphql',
   cache: new InMemoryCache()
 });
+*/
 
 
 const router = createBrowserRouter([
--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SIGNUP_USER = gql`
+  mutation Signup($username: String!, $password: String!) {
+    signup(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 // defines GraphQL mutation to add subcategory to budget category
 export const ADD_SUBCATEGORY = gql`
   mutation AddSubcategory($category: String!, $name: String!, $amount: Float!) {
